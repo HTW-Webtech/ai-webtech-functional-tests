4574486ac0a003b565154bd7fe1eecca5598c905
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-11-03 12:46:38 +0100

    Tweak w3c validator spec

--- spec/support/validator_helper.rb
+++ spec/support/validator_helper.rb
@@ -1,7 +1,10 @@
 module ValidatorHelper
-  VALIDATOR_URL = 'https://validator.w3.org/nu/?showsource=yes&useragent=Validator.nu%2FLV+http%3A%2F%2Fvalidator.w3.org%2Fservices&doc={URL}'
-
-  def visit_validation_page(url)
-    visit VALIDATOR_URL.gsub('{URL}', url)
+  def page_markup_valid?(url)
+    visit url
+    markup = page.body
+    visit 'https://validator.w3.org/#validate_by_input'
+    fill_in 'fragment', with: markup
+    click_on 'Check'
+    !!page.body.match(/Document checking completed. No errors or warnings to show./)
   end
 end
