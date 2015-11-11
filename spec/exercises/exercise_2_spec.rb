6b5dd88a4a920afba6f7c36290228b33044ade0f
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-11-11 16:01:14 +0100

    Finetune error messages for 2nd exercise spec

--- spec/exercises/exercise_2_spec.rb
+++ spec/exercises/exercise_2_spec.rb
@@ -1,9 +1,9 @@
 require 'open-uri'
 
-describe 'Exercise 2: CSS Layouts', type: :feature, js: true do
+describe 'Exercise 2:', type: :feature, js: true do
   INDEX_URL = $EXERCISE_BASE_URL
 
-  context 'on the index.html' do
+  context 'The index.html' do
     before { visit INDEX_URL }
 
     it 'includes a link-tag with rel="stylesheet"' do
@@ -14,16 +14,16 @@ describe 'Exercise 2: CSS Layouts', type: :feature, js: true do
       page.find(:xpath, "//head/link[contains(@href, 'style.css')]", visible: false)
     end
 
-    context 'the style.css' do
+    context 'includes a style.css' do
       let(:style_node) { page.find(:xpath, "//head/link[contains(@href, 'style.css')]", visible: false) }
       let(:style_url) { "#{INDEX_URL}/#{style_node['href']}" }
       let(:style) { open(style_url).read }
 
       %w[background border border-radius color display content font-family
       font-size font-weight margin padding transition transform].each do |expected_property|
-        it "includes the css property #{expected_property}" do
+        it ", which contains the css property #{expected_property}" do
           expected = "#{expected_property}:"
-          expect(style.include?(expected)).to eq(true), "Your style.css does not include: '#{expected}'"
+          expect(style.include?(expected)).to eq(true), "Your style.css does not include the css-property '#{expected_property}'"
         end
       end
     end
