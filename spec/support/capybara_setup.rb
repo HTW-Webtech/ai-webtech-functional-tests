4176b5ff74e4e779d16e617f17028d88dec82e4d
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-11-11 15:43:33 +0100

    Configure poltergeist for javascript tests in production

--- spec/support/capybara_setup.rb
+++ spec/support/capybara_setup.rb
@@ -1,6 +1,7 @@
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
 
 if ENV['TEST_ENV'] == 'development'
   Capybara.register_driver :selenium_chrome do |app|
