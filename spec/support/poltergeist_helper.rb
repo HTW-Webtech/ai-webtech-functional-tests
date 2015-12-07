c16f56877a208c8b61d631448f3210589dd7c337
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-12-07 17:29:11 +0100

    Remove localStorage debugging output

--- spec/support/poltergeist_helper.rb
+++ spec/support/poltergeist_helper.rb
@@ -1,6 +1,5 @@
 module PoltergeistHelper
   def clear_local_storage
     page.execute_script('if (localStorage && localStorage.clear) localStorage.clear();')
-    page.evaluate_script 'localStorage'
   end
 end
