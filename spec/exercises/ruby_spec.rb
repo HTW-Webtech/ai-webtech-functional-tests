e4f19bbe29c6ef626ea58ef8850d6aeee234361e
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-08 09:51:01 +0200

    Handle failure case with positive expection

--- spec/exercises/ruby_spec.rb
+++ spec/exercises/ruby_spec.rb
@@ -8,8 +8,9 @@ describe 'Ruby Exercise' do
   it 'runs all koans successfully' do
     system "test -e #{GIT_TARGET_PATH} || git clone #{GIT_REPO_PATH} #{GIT_TARGET_PATH}"
     FileUtils.cd GIT_TARGET_PATH do
-      result = `rake`.include?("meditate on the following code")
-      expect(result).to eq(false), "You did not finish all the koans yet."
+      result = `rake`
+      expect(result).to include("Mountains are again merely mountains"), "You did not finish all the koans yet."
+      expect(result).to_not include("meditate on the following code"), "You did not finish all the koans yet."
     end
   end
 end
