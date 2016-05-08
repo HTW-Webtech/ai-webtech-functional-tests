36517e22011eaa1e438f6564f5c5b9e8e9c1112f
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-08 10:01:38 +0200

    Ruby Exercise: Fix clone target path

--- spec/exercises/ruby_spec.rb
+++ spec/exercises/ruby_spec.rb
@@ -3,7 +3,7 @@ describe 'Ruby Exercise' do
   GIT_TARGET_PATH = "./vendor/ruby_koans_code"
 
   it 'runs all koans successfully' do
-    system "test -e #{GIT_REPO_PATH} || git clone #{GIT_REPO_PATH} #{GIT_TARGET_PATH}"
+    system "test -e #{GIT_TARGET_PATH} || git clone #{GIT_REPO_PATH} #{GIT_TARGET_PATH}"
     FileUtils.cd GIT_TARGET_PATH do
       system "git pull origin master"
     end
