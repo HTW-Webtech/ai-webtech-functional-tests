5c157a1b7767a8669b9d178768585b3ec12de61a
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-08 13:22:09 +0200

    FindHelper: use #expect_xpath instead #find_xpath

--- spec/exercises/html_spec.rb
+++ spec/exercises/html_spec.rb
@@ -1,29 +1,29 @@
 shared_examples 'all pages and' do |url|
-  let(:url) { url }
+  before { visit url }
 
   it 'includes the product-logo' do
-    find_xpath(tag: 'img', attr: 'src', value: 'bazinga-logo.png')
+    expect_xpath(tag: 'img', attr: 'src', value: 'bazinga-logo.png')
   end
 
   it 'has the correct headline' do
-    find_xpath(tag: 'h1', value: 'Bazinga!')
+    expect_xpath(tag: 'h1', value: 'Bazinga!')
   end
 
   it 'links to "index.html"' do
-    find_xpath(tag: 'a', attr: 'href', value: 'index.html')
+    expect_xpath(tag: 'a', attr: 'href', value: 'index.html')
   end
 
   it 'links to "contact.html"' do
-    find_xpath(tag: 'a', attr: 'href', value: 'contact.html')
+    expect_xpath(tag: 'a', attr: 'href', value: 'contact.html')
   end
 
   it 'links to "imprint.html"' do
-    find_xpath(tag: 'a', attr: 'href', value: 'imprint.html')
+    expect_xpath(tag: 'a', attr: 'href', value: 'imprint.html')
   end
 
   it 'has the required meta-tags for `author` and `keywords`' do
-    find_xpath(tag: 'meta', attr: 'name', value: 'author')
-    find_xpath(tag: 'meta', attr: 'name', value: 'keywords')
+    expect_xpath(tag: 'meta', attr: 'name', value: 'author')
+    expect_xpath(tag: 'meta', attr: 'name', value: 'keywords')
   end
 
   # FIXME: Does not accept meta tag for utf8 charset
@@ -42,30 +42,30 @@ describe 'HTML Übung:', type: :feature do
     before { visit INDEX_URL }
 
     it 'has a p-tag containing the Bazinga product message' do
-      find_xpath(tag: 'p', value: 'Bazinga! Focus on getting your software')
+      expect_xpath(tag: 'p', value: 'Bazinga! Focus on getting your software')
     end
   end
 
   context 'contact.html' do
-    it_behaves_like 'all pages and', CONTACT_URL
+    it_behaves_like 'all pages and'
     before { visit CONTACT_URL }
 
     it 'has a form with action="/contact", and method="/post"' do
-      find_xpath(tag: 'form', attr: 'method', value: 'post')
-      find_xpath(tag: 'form', attr: 'action', value: '/contact')
+      expect_xpath(tag: 'form', attr: 'method', value: 'post')
+      expect_xpath(tag: 'form', attr: 'action', value: '/contact')
     end
   end
 
   context 'imprint.html' do
-    it_behaves_like 'all pages and', IMPRINT_URL
+    it_behaves_like 'all pages and'
     before { visit IMPRINT_URL }
 
     it 'includes the telephon-number as anchor' do
-      find_xpath(tag: 'a', attr: 'href', value: 'tel:03050190')
+      expect_xpath(tag: 'a', attr: 'href', value: 'tel:03050190')
     end
 
     it 'includes the address in a proper <address>-Tag' do
-      find_xpath(tag: 'address', value: 'Wilhelminenhofstraße 75A')
+      expect_xpath(tag: 'address', value: 'Wilhelminenhofstraße 75A')
     end
   end
 end
--- spec/support/find_helper.rb
+++ spec/support/find_helper.rb
@@ -1,9 +1,9 @@
 module FindHelper
-  def find_xpath(tag:, attr: '.', value:)
+  def expect_xpath(tag:, attr: '.', value:)
     if attr == '.'
-      expect(page).to have_selector(:xpath, "//#{tag}[contains(#{attr}, '#{value}')]"), "<#{tag}>#{value}</#{tag}> nicht gefunden."
+      expect(page).to have_selector(:xpath, "//#{tag}[contains(#{attr}, '#{value}')]", visible: false), "<#{tag}>#{value}</#{tag}> nicht gefunden."
     else
-      expect(page).to have_selector(:xpath, "//#{tag}[contains(@#{attr}, '#{value}')]"), "<#{tag} #{attr}='#{value}'> nicht gefunden."
+      expect(page).to have_selector(:xpath, "//#{tag}[contains(@#{attr}, '#{value}')]", visible: false), "<#{tag} #{attr}='#{value}'> nicht gefunden."
     end
   end
 end
