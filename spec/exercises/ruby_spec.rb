17dc6361ee1d02c1f16594714515d69f65585c0a
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-08 10:00:30 +0200

    Ensure latest code

--- spec/exercises/ruby_spec.rb
+++ spec/exercises/ruby_spec.rb
@@ -3,7 +3,11 @@ describe 'Ruby Exercise' do
   GIT_TARGET_PATH = "./vendor/code"
 
   it 'runs all koans successfully' do
-    system "test -e #{GIT_TARGET_PATH} || git clone #{GIT_REPO_PATH} #{GIT_TARGET_PATH}"
+    system "test -e #{GIT_REPO_PATH} || git clone #{GIT_REPO_PATH} #{GIT_TARGET_PATH}"
+    FileUtils.cd GIT_TARGET_PATH do
+      system "git pull origin master"
+    end
+
     FileUtils.cd GIT_TARGET_PATH do
       result = `rake`
       expect(result).to include("Mountains are again merely mountains"), "You did not finish all the koans yet."
