8a7b78ce773175b74f4371da38da1d0ff2ffef30
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-10 17:04:06 +0200

    Ruby-Spec: git reset instead stashing

--- spec/exercises/ruby_spec.rb
+++ spec/exercises/ruby_spec.rb
@@ -5,7 +5,7 @@ describe 'Ruby Exercise' do
   it 'runs all koans successfully' do
     system "test -e #{GIT_TARGET_PATH} || git clone #{GIT_REPO_PATH} #{GIT_TARGET_PATH}"
     FileUtils.cd GIT_TARGET_PATH do
-      system "git stash && git pull origin master && git stash drop"
+      system "git reset HEAD --hard && git pull origin master"
     end
 
     FileUtils.cd GIT_TARGET_PATH do
