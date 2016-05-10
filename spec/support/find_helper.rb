bf3ce6de1238caed4ce5399cd4fe8376a912f761
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-10 07:10:08 +0200

    FindHelper: Use a lower default wait time

--- spec/support/find_helper.rb
+++ spec/support/find_helper.rb
@@ -1,9 +1,9 @@
 module FindHelper
   def expect_xpath(tag:, attr: '.', value:)
     if attr == '.'
-      expect(page).to have_selector(:xpath, "//#{tag}[contains(#{attr}, '#{value}')]", visible: false), "<#{tag}>#{value}</#{tag}> nicht gefunden."
+      expect(page).to have_selector(:xpath, "//#{tag}[contains(#{attr}, '#{value}')]", visible: false, wait: 0.1), "<#{tag}>#{value}</#{tag}> nicht gefunden."
     else
-      expect(page).to have_selector(:xpath, "//#{tag}[contains(@#{attr}, '#{value}')]", visible: false), "<#{tag} #{attr}='#{value}'> nicht gefunden."
+      expect(page).to have_selector(:xpath, "//#{tag}[contains(@#{attr}, '#{value}')]", visible: false, wait: 0.1), "<#{tag} #{attr}='#{value}'> nicht gefunden."
     end
   end
 end
