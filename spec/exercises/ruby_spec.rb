822d1991401ebd8e3a33b6cd54240f3d1e325046
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-18 17:06:53 +0200

    Ruby-Exercise: Fix git expectation

--- spec/exercises/ruby_spec.rb
+++ spec/exercises/ruby_spec.rb
@@ -4,12 +4,13 @@ describe 'Ruby Exercise' do
 
   it 'runs all koans successfully' do
     system "test -e #{GIT_TARGET_PATH} || git clone #{GIT_REPO_PATH} #{GIT_TARGET_PATH}"
-    File.exists? GIT_TARGET_PATH or fail("Das git Repository konnte nicht gefunden werden. Sind wirklich alle Änderungen auf dem Aris-Server?")
+
     FileUtils.cd GIT_TARGET_PATH do
       puts "PWD: #{`pwd`.chomp}"
       system "git reset HEAD --hard && git pull origin master"
     end
 
+    File.exists? "#{GIT_TARGET_PATH}/Rakfile" or fail("Das git Repository enthält kein Rakefile, dass zur Übung gehört. Sind wirklich alle Änderungen auf dem Aris-Server?")
     FileUtils.cd GIT_TARGET_PATH do
       result = `rake`
       expect(result).to include("Mountains are again merely mountains"), "You did not finish all the koans yet."
